62d0e7336695f8fc894f7c95c074a1a455d858e5
Adrian <48665902+acmatias@users.noreply.github.com>
2022-03-17 15:21:44 +1100

    about section

---
+++ src/pages/AboutUs.js
@@ -0,0 +1,11 @@
+import React from 'react'
+//page components
+import { AboutSection } from '../components/AboutSection'
+
+export const AboutUs = () => {
+    return (
+        <div className="">
+            <AboutSection />
+        </div>
+    )
+}
